902d24726d56c3354c66ff1e0076c859992c27d4
Yang Zhou <yangzhouinter@gmail.com>
2025-03-25 19:17:43 +1100

    add two more functions for the agent.

--- src/tools.ts
+++ src/tools.ts
@@ -9,14 +9,62 @@ export const tools: Tool[] = [
     functionDeclarations: [
       {
         name: "getDate",
-        description: "Get the current date",
+        description: "Get the current date and use it to workout other days",
+      },
+      {
+        name: "add",
+        description:
+          "Add two numbers together. Use this for accurate addition.",
+        parameters: {
+          type: SchemaType.OBJECT,
+          description: "The numbers to add together",
+          required: ["a", "b"],
+          properties: {
+            a: {
+              type: SchemaType.NUMBER,
+              description: "The first number",
+            },
+            b: {
+              type: SchemaType.NUMBER,
+              description: "The second number",
+            },
+          },
+        },
+      },
+      {
+        name: "multiply",
+        description:
+          "Multiply two numbers together. Use this for accurate multiplication.",
+        parameters: {
+          type: SchemaType.OBJECT,
+          description: "The numbers to multiply together",
+          required: ["a", "b"],
+          properties: {
+            a: {
+              type: SchemaType.NUMBER,
+              description: "The first number",
+            },
+            b: {
+              type: SchemaType.NUMBER,
+              description: "The second number",
+            },
+          },
+        },
       },
     ],
   },
 ];
 
-export const functions = { getDate };
+export const functions = { getDate, add, multiply };
 
 function getDate() {
   return { date: new Date().toDateString() };
+}
+
+function add({ a, b }: { a: number; b: number }) {
+  return { additionResult: a + b };
+}
+
+function multiply({ a, b }: { a: number; b: number }) {
+  return { multiplicationResult: a * b };
 }
