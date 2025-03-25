0d98f0b6cf2a6a36777d84768d0168fd4dcc08b6
Phil Nash <philnash@gmail.com>
2025-03-25 16:39:45 +1100

    Basic bot and instructions

---
+++ src/tools.ts
@@ -0,0 +1,9 @@
+import { Tool, SchemaType, FunctionCallingMode } from "@google/generative-ai";
+import { genAI } from "./bot.js";
+
+export const tools: Tool[] = [
+  {
+    functionDeclarations: [],
+  },
+];
+export const functions = {};
